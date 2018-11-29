11620d64ee81fae385c29578af8178429e2c487d
Andrei Bodnar <bodnar1212@gmail.com>
2018-11-29 10:31:15 +0200

    fix minor

--- sample/bootstrap.php
+++ sample/bootstrap.php
@@ -371,3 +371,38 @@ $hubSpotService->setAuthenticationData(new \BrizyForms\Model\AuthenticationData(
 var_dump($hubSpotService->getFields());
 
 $hubSpotService->createMember($fieldMap, null, $dataArray);
+
+//create $mailerLiteService service
+
+$fields   = '[{"source_id":"1", "source_title":"Email", "target":"email"}, {"source_id":"2", "source_title":"My Name", "target":"_auto_generate"}]';
+$fieldMap = new \BrizyForms\FieldMap(json_decode($fields, true));
+
+$data = '[{"name":"2","value":"Anthony","required":false,"type":"text","slug":"name"},{"name":"1","value":"bodnar121211111111@gmail.com","required":false,"type":"email","slug":"email"}]';
+$data = json_decode($data, true);
+
+$dataArray = [];
+foreach ($data as $row) {
+    $data = new \BrizyForms\Model\Data();
+    $data
+        ->setName($row['name'])
+        ->setValue($row['value']);
+    $dataArray[] = $data;
+}
+
+$mailerLiteService = \BrizyForms\ServiceFactory::getInstance(\BrizyForms\ServiceFactory::MAILERLITE);
+
+$mailerLiteService->setAuthenticationData(new \BrizyForms\Model\AuthenticationData([
+    'api_key' => 'b37a47c4374f5917a49805dca0230651'
+]));
+
+$groups = $mailerLiteService->getGroups();
+
+$active_group = null;
+foreach ($groups as $group) {
+    var_dump($group);
+    $active_group = $group->getId();
+}
+
+var_dump($mailerLiteService->getFields());
+
+$mailerLiteService->createMember($fieldMap, $active_group, $dataArray);
